37c53f05d87b3ce3a9af3f94ce95809fe9095a60
Roman <roma.eksit@gmail.com>
2026-06-06 03:16:55 +0200

    Fix variable shadowing in cinema_visit function

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ from app.people.customer import Customer
 def cinema_visit(customers: list, hall_number: int,
                  cleaner: str, movie: str) -> None:
     cinema_hall = CinemaHall(hall_number)
-    cleaner = Cleaner(cleaner)
-    customers = [
+    cleaner_entity = Cleaner(cleaner)
+    customer_entities = [
         Customer(customer["name"], customer["food"])
         for customer in customers
     ]
 
-    for customer in customers:
+    for customer in customer_entities:
         CinemaBar.sell_product(customer.food, customer)
-    cinema_hall.movie_session(movie, customers, cleaner)
+    cinema_hall.movie_session(movie, customer_entities, cleaner_entity)
