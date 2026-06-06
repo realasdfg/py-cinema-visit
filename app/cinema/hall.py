ecbf519d73da78791c6868e4210888f8bee435e9
Roman <roma.eksit@gmail.com>
2026-06-06 03:06:56 +0200

    Change CinemaHall parameter back to number

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -3,13 +3,13 @@ from app.people.customer import Customer
 
 
 class CinemaHall:
-    def __init__(self, hall_number: int) -> None:
-        self.hall_number = hall_number
+    def __init__(self, number: int) -> None:
+        self.number = number
 
     def movie_session(self, movie_name: str, customers: list[Customer],
                       cleaning_staff: Cleaner) -> None:
-        print(f'"{movie_name}" started in hall number {self.hall_number}.')
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
-        cleaning_staff.clean_hall(self.hall_number)
+        cleaning_staff.clean_hall(self.number)
